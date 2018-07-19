0fc3afe7187f2ba5ab9d961eb1be76b48fbfb6ea
Fonsik <fonsik97@gmail.com>
2018-07-19 11:25:42 +0200

    ddd

--- gener.py
+++ gener.py
@@ -1,7 +1,21 @@
+import random
+from termcolor import colored
 
-n, m = 28, 28;
+random.seed()
+
+m=28
+n=28
 Matrix = [[0 for x in range(n)] for y in range(m)] 
+
+
+a=random.uniform(-1,1)
+b=random.uniform(0,28)
+
+for x in range (28):
+	y=a*x+b
+	y=int(y)
+	if (y<28and y>=0):
+		Matrix[x][y]="*"
 
 for x in range (n):
 	print Matrix[x]
-
